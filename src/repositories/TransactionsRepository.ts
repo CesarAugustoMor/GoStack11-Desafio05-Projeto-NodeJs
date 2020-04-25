38004ae9586703ceacd16cd401b92929f32479ae
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-25 16:56:17 -0300

    :sparkles: Resolução do Desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,12 +1,17 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
   total: number;
 }
 
-class TransactionsRepository {
+export default class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
@@ -14,16 +19,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acumulator: Balance, transaction) => {
+        const acum = acumulator;
+        switch (transaction.type) {
+          case 'income':
+            acum.income += transaction.value;
+            acum.total += transaction.value;
+            break;
+          case 'outcome':
+            acum.outcome += transaction.value;
+            acum.total -= transaction.value;
+            break;
+
+          default:
+            break;
+        }
+        return acum;
+      },
+      { income: 0, outcome: 0, total: 0 },
+    );
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
-
-export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,33 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
-class CreateTransactionService {
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+export default class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['include', 'outcome'].includes(type)) {
+      throw new Error('Type invalid');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enougth balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
   }
 }
-
-export default CreateTransactionService;
